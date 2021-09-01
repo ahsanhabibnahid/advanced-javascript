2d239d3ee8813e11ccd462ff403479393bdb0e96
ahsanhabibnahid <ahsanhabib3239@gmail.com>
2021-09-01 12:04:57 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+let age;
+console.log(age) // undefined
+
+function sum(x,y){
+    console.log(x + y)
+}
+
+const result = sum(10,65)
+console.log(result) // undefined
+
+
+const person = { name : 'nahid', age : 25, pl : 'javascript' }
+console.log(person.address)
+
+let result = null;
+console.log(result)
