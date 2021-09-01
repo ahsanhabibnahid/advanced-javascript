f76488d834142d5cc7329298a2e0415645b55b9c
ahsanhabibnahid <ahsanhabib3239@gmail.com>
2021-09-01 14:45:11 +0600

    == and ===

---
+++ double-triple-equal.js
@@ -0,0 +1,17 @@
+const first = 0
+const second = false
+
+if (first == second) {
+    console.log('condition is true')
+} else {
+    console.log('condition is false')
+}
+
+const third = 0
+const fourth = false
+
+if (third === fourth) {
+    console.log('condition is true')
+} else {
+    console.log('condition is false')
+}
